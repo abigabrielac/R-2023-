677364db0559fda64846420ee488ab6364f32d7c
César Núñez <75745356+cesarnunezh@users.noreply.github.com>
2023-01-24 19:35:02 -0500

    Create Evaluación 2.R

---
+++ Evaluación 2.R
@@ -0,0 +1,15 @@
+# Pr??ctica Calificada N° 2 - Grupo 4 ----
+# Integrantes:
+# - Gabriela Calvo
+# - Mauricio Ib????ez
+# - C??sar N????ez
+# - Cristian Orellana
+
+# PREGUNTA 1 ----
+## PREGUNTA 1.A.----
+
+## PREGUNTA 1.B.----
+
+
+# PREGUNTA 2 ----
+
